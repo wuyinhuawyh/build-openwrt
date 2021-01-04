a08cdd9c60b8dba11ac2f94bf058568ad746dcb3
yichu77 <yichu77@qq.com>
2021-01-04 14:46:03 +0800

    upload Project_newifi_mini

---
+++ build/Project_newifi_mini/diy-1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# 说明：
+# 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
+
+
+#源码里面几乎囊括了所有插件，所以基本不需要添加插件了
+#git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan #微信推送
+git clone https://github.com/zzsj0928/luci-app-serverchand.git package/lean/luci-app-serverchand #钉钉推送
+#git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/lean/luci-theme-opentopd #主题
+#git clone https://github.com/sirpdboy/luci-app-advanced.git package/lean/luci-app-advanced #高级设置
+#git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
+git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
+
+#rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
+#全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
+#用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
